41b3f40113af75e1f36cf68192a4a7951d769514
Jiří Jareš <info@chillapartments.onmicrosoft.com>
2025-08-06 08:53:16 +0200

    Fixed vite.config.js export for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-base: '/',
-export default defineConfig({
-  plugins: [react()]
-})
+const { defineConfig } = require('vite');
+
+module.exports = defineConfig({
+  base: "/",
+});
+
